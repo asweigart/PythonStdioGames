802b17f46afe4bf5029ad86e6ddda073db56d6ea
Al Sweigart <asweigart@gmail.com>
2018-12-04 18:30:15 -0800

    Added sliding puzzle.

---
+++ src/slidingpuzzle.py
@@ -0,0 +1,114 @@
+import random
+
+BLANK = '  '
+
+
+def getNewBoard():
+    return [['1 ', '5 ', '9 ', '13'], ['2 ', '6 ', '10', '14'], ['3 ', '7 ', '11', '15'], ['4 ', '8 ', '12', BLANK]]
+
+
+def drawBoard(board):
+    labels = [board[0][0], board[1][0], board[2][0], board[3][0],
+              board[0][1], board[1][1], board[2][1], board[3][1],
+              board[0][2], board[1][2], board[2][2], board[3][2],
+              board[0][3], board[1][3], board[2][3], board[3][3]]
+    print("""
++----+----+----+----+
+|    |    |    |    |
+| {} | {} | {} | {} |
+|    |    |    |    |
++----+----+----+----+
+|    |    |    |    |
+| {} | {} | {} | {} |
+|    |    |    |    |
++----+----+----+----+
+|    |    |    |    |
+| {} | {} | {} | {} |
+|    |    |    |    |
++----+----+----+----+
+|    |    |    |    |
+| {} | {} | {} | {} |
+|    |    |    |    |
++----+----+----+----+
+""".format(*labels))
+
+
+def findBlankSpace(board):
+    for x in range(4):
+        for y in range(4):
+            if board[x][y] == '  ':
+                return (x, y)
+
+
+def getPlayerMove(board):
+    blankx, blanky = findBlankSpace(board)
+
+    w = 'W' if blanky != 3 else ' '
+    a = 'A' if blankx != 3 else ' '
+    s = 'S' if blanky != 0 else ' '
+    d = 'D' if blankx != 0 else ' '
+
+    while True:
+        print('                   %s' % w)
+        print('Enter your move: %s %s %s' % (a, s, d))
+
+        move = input().upper()
+        if move in (w + a + s + d).replace(' ', ''):
+            return move
+
+
+def makeMove(board, move):
+    # Note: This function assumes that the move is valid.
+    blankx, blanky = findBlankSpace(board)
+
+    if move == 'W':
+        board[blankx][blanky], board[blankx][blanky + 1] = board[blankx][blanky + 1], board[blankx][blanky]
+    elif move == 'A':
+        board[blankx][blanky], board[blankx + 1][blanky] = board[blankx + 1][blanky], board[blankx][blanky]
+    elif move == 'S':
+        board[blankx][blanky], board[blankx][blanky - 1] = board[blankx][blanky - 1], board[blankx][blanky]
+    elif move == 'D':
+        board[blankx][blanky], board[blankx - 1][blanky] = board[blankx - 1][blanky], board[blankx][blanky]
+
+
+def makeRandomMove(board):
+    blankx, blanky = findBlankSpace(board)
+    validMoves = []
+    if blanky != 3:
+        validMoves.append('W')
+    if blankx != 3:
+        validMoves.append('A')
+    if blanky != 0:
+        validMoves.append('S')
+    if blankx != 0:
+        validMoves.append('D')
+
+    makeMove(board, random.choice(validMoves))
+
+
+def main():
+    print('Sliding Puzzle')
+    print('Use the WASD keys to move the tiles')
+    print('back into their original order:')
+    print('       1  2  3  4')
+    print('       5  6  7  8')
+    print('       9 10 11 12')
+    print('      13 14 15   ')
+    print()
+
+    gameBoard = getNewBoard()
+    for i in range(200):
+        makeRandomMove(gameBoard)
+
+
+    while True:
+        drawBoard(gameBoard)
+        playerMove = getPlayerMove(gameBoard)
+        makeMove(gameBoard, playerMove)
+
+        if gameBoard == getNewBoard():
+            print('You won!')
+
+
+if __name__ == '__main__':
+    main()
