c9156979bfe41e827a6d7450ec5c03b729d50c38
Al Sweigart <asweigart@gmail.com>
2019-01-03 11:36:41 -0600

    Fixed bug in DNA animation.

--- src/dna.py
+++ src/dna.py
@@ -30,6 +30,11 @@ time.sleep(2)
 rowIndex = 0
 
 while True: # Main animation loop.
+    # Increment rowIndex to draw next row:
+    rowIndex += 1
+    if rowIndex == len(rows):
+        rowIndex = 0
+
     # Row indexs 0 and 9 don't have nucleotides:
     if rowIndex == 0 or rowIndex == 9:
         print(rows[rowIndex])
@@ -50,8 +55,3 @@ while True: # Main animation loop.
             print(rows[rowIndex].format('G', 'C'))
 
     time.sleep(0.15) # Add a slight pause.
-
-    # Increment rowIndex to draw next row:
-    rowIndex += 1
-    if rowIndex == len(rows):
-        rowIndex = 0
