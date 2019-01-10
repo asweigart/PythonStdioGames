e800857c70c59f389847a457896002a147f3a72e
Al Sweigart <asweigart@gmail.com>
2019-01-10 13:04:19 -0800

    Adding "dice stats" program.

---
+++ src/dicestats.py
@@ -0,0 +1,26 @@
+# Dice Stats, by Al Sweigart al@inventwithpython.com
+
+import random
+
+print('Dice Statistics Simulator')
+print()
+print('Enter how many six-sided dice you want to roll:')
+numberOfDice = int(input())
+
+# Set up dictionary to store results:
+results = {}
+for i in range(numberOfDice, (numberOfDice * 6) + 1):
+    results[i] = 0
+
+# Simulate dice rolls.
+print('Simulating 1,000,000 dice rolls...')
+for i in range(1000000):
+    total = 0
+    for j in range(numberOfDice):
+        total = total + random.randint(1, 6)
+    results[total] = results[total] + 1
+
+# Display results:
+print('Results:')
+for i in range(numberOfDice, (numberOfDice * 6) + 1):
+    print('  %s - %s rolls - %s%%' % (i, results[i], round(results[i] / 10000, 1)))
