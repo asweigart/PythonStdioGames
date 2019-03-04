ff62359410a5528bec708f8844d8ec301a1e0cde
Al Sweigart <asweigart@gmail.com>
2019-03-04 14:04:49 -0800

    Adding diagonal maze

---
+++ src/diagonalmaze.py
@@ -0,0 +1,14 @@
+# Prints out a random, diagonal maze. By Al Sweigart
+# Inspired by the 10 PRINT CHR$(205.5+RND(1)); : GOTO 10 program.
+
+import random
+
+FORWARD = chr(int('2571', 16)) # The ╱ character.
+BACK    = chr(int('2572', 16)) # The ╲ character.
+
+for x in range(2000): # Loop for each slash in the current line.
+    # Randomly add a forward or back slash to the line.
+    if random.randint(0, 1) == 0:
+        print(FORWARD, end='')
+    else:
+        print(BACK, end='')
