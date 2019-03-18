0ecc732f0d3c1d397756793c7657239d525a36e0
Al Sweigart <asweigart@gmail.com>
2019-03-18 00:40:04 -0700

    Polishing "rotating cube"

--- src/rotating_cube.py
+++ src/rotating_cube.py
@@ -1,34 +1,22 @@
-# Rotating Cube, by Al Sweigart
+# Rotating Cube, by Al Sweigart al@inventwithpython.com
 
-import math, time, random, sys
-assert sys.version_info.major == 3, 'Run this program on Python 3.'
+import math, time, random, sys, os
 
-try:
-    import bext
-except:
-    sys.exit('Bext is required to run this. Run `pip install bext` from the shell to install it.')
-
-
-BLOCK = chr(9608)
-
-# Resize the cube based on the size of the terminal window.
-width, height = bext.size()
-DEFAULT_SCALEX = (width - 4) // 8
-DEFAULT_SCALEY = (height - 4) // 4 # Text cells are twice as tall as they are wide, so set scaley accordingly.
-DEFAULT_TRANSLATEX = (width - 4) // 2
-DEFAULT_TRANSLATEY = (height - 4) // 2
+if len(sys.argv) == 3:
+    # Set size based on command line arguments:
+    WIDTH = int(sys.argv[1])
+    HEIGHT = int(sys.argv[2])
+else:
+    WIDTH, HEIGHT = 80, 50
+DEFAULT_SCALEX = (WIDTH - 4) // 8
+DEFAULT_SCALEY = (HEIGHT - 4) // 4 # Text cells are twice as tall as they are wide, so set scaley accordingly.
+DEFAULT_TRANSLATEX = (WIDTH - 4) // 2
+DEFAULT_TRANSLATEY = (HEIGHT - 4) // 2
 
 
 def line(x1, y1, x2, y2):
-    """
-    Returns a generator that produces all of the points in a line between `x1`, `y1` and `x2`, `y2`.
-
-    (Uses the Bresenham line algorithm.)
-
-    >>> list(line(0, 0, 10, 3))
-    [(0, 0), (1, 0), (2, 1), (3, 1), (4, 1), (5, 1), (6, 2), (7, 2), (8, 2), (9, 3), (10, 3)]
-    """
-    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # TODO - Do we want this line?
+    """Returns a generator that produces all of the points in a line
+    between `x1`, `y1` and `x2`, `y2`. Uses the Bresenham line algorithm."""
 
     isSteep = abs(y2-y1) > abs(x2-x1)
     if isSteep:
@@ -112,14 +100,12 @@ def transformPoint(point1, point2, scalex=None, scaley=None, translatex=None, tr
     if translatey is None:
         translatey = DEFAULT_TRANSLATEY
 
-
     return (int(point1[0] * scalex + translatex),
             int(point1[1] * scaley + translatey),
             int(point2[0] * scalex + translatex),
             int(point2[1] * scaley + translatey))
 
 
-bext.fg('random')
 
 # Set up the points of the cube:
 '''
@@ -142,58 +128,57 @@ points = [[-1, -1, -1],
           [-1,  1,  1],
           [ 1,  1,  1]]
 rotatedPoints = [None] * 10
-rx = ry = rz = 0
-step = 0
+rx = ry = rz = 0 # Rotation amounts for each axis.
 
-bext.clear()
 try:
     while True:
-        # Change color every 15 steps:
-        if step % 15 == 0:
-            bext.fg('random')
-        step += 1
-
-        # Rotate cube:
+        # Rotate the cube:
         rx += 0.03 + random.randint(1, 20) / 100
         ry += 0.08  + random.randint(1, 20) / 100
         rz += 0.13 + random.randint(1, 20) / 100
         for i in range(len(points)):
             rotatedPoints[i] = rotateXYZ(*points[i], rx, ry, rz)
 
-        # Get points of the cube lines:
-        screenPoints = []
-        screenPoints.extend(line(*transformPoint(rotatedPoints[0], rotatedPoints[1])))
-        screenPoints.extend(line(*transformPoint(rotatedPoints[1], rotatedPoints[3])))
-        screenPoints.extend(line(*transformPoint(rotatedPoints[3], rotatedPoints[2])))
-        screenPoints.extend(line(*transformPoint(rotatedPoints[2], rotatedPoints[0])))
+        # Get the points of the cube lines:
+        cubePoints = []
+        cubePoints.extend(line(*transformPoint(rotatedPoints[0], rotatedPoints[1])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[1], rotatedPoints[3])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[3], rotatedPoints[2])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[2], rotatedPoints[0])))
 
-        screenPoints.extend(line(*transformPoint(rotatedPoints[0], rotatedPoints[4])))
-        screenPoints.extend(line(*transformPoint(rotatedPoints[1], rotatedPoints[5])))
-        screenPoints.extend(line(*transformPoint(rotatedPoints[2], rotatedPoints[6])))
-        screenPoints.extend(line(*transformPoint(rotatedPoints[3], rotatedPoints[7])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[0], rotatedPoints[4])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[1], rotatedPoints[5])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[2], rotatedPoints[6])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[3], rotatedPoints[7])))
 
-        screenPoints.extend(line(*transformPoint(rotatedPoints[4], rotatedPoints[5])))
-        screenPoints.extend(line(*transformPoint(rotatedPoints[5], rotatedPoints[7])))
-        screenPoints.extend(line(*transformPoint(rotatedPoints[7], rotatedPoints[6])))
-        screenPoints.extend(line(*transformPoint(rotatedPoints[6], rotatedPoints[4])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[4], rotatedPoints[5])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[5], rotatedPoints[7])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[7], rotatedPoints[6])))
+        cubePoints.extend(line(*transformPoint(rotatedPoints[6], rotatedPoints[4])))
 
-        screenPoints = tuple(frozenset(screenPoints)) # Get rid of duplicate points.
+        cubePoints = tuple(frozenset(cubePoints)) # Get rid of duplicate points.
 
-        # Draw cube:
-        for x, y in screenPoints:
-            # (Writing to the terminal will by far be the slowest part of this program.)
-            bext.goto(x, y)
-            print(BLOCK, end='')
+        # Draw the cube:
+        for y in range(0, HEIGHT, 2):
+            for x in range(WIDTH):
+                if (x, y) in cubePoints and (x, y + 1) in cubePoints:
+                    print(chr(9608), end='', flush=False) # Draw full block.
+                elif (x, y) in cubePoints and (x, y + 1) not in cubePoints:
+                    print(chr(9600), end='', flush=False) # Draw top half of block.
+                elif not (x, y) in cubePoints and (x, y + 1) in cubePoints:
+                    print(chr(9604), end='', flush=False) # Draw bottom half of block.
+                else:
+                    print(' ', end='', flush=False) # Draw empty space.
+            print()
+        print('', end='', flush=True)
 
-        time.sleep(0.2) # Pause for a bit.
+        time.sleep(0.1) # Pause for a bit.
 
-        # Erase cube:
-        for x, y in screenPoints:
-            bext.goto(x, y)
-            print(' ', end='')
+        # Erase the screen:
+        if sys.platform == 'win32':
+            os.system('cls')
+        else:
+            os.system('clear')
 
-        # Print quit message:
-        bext.goto(0, height - 1)
-        print('Press Ctrl-C to quit.', end='')
 except KeyboardInterrupt:
-    pass
+    pass # When Ctrl-C is pressed, stop looping.
