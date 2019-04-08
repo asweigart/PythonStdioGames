03f187cbe3089a741137bca3dfd61fc0e493d664
Al Sweigart <asweigart@gmail.com>
2019-04-07 22:15:58 -0700

    Adding rot13

---
+++ src/rot13.py
@@ -0,0 +1,44 @@
+# ROT13 Cipher by Al Sweigart, al@inventwithpython.com
+# More info at https://en.wikipedia.org/wiki/ROT13
+
+try:
+    import pyperclip
+except ImportError:
+    pass # Don't do anything if pyperclip fails to import.
+
+upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
+
+while True: # Main program loop.
+    print('Enter a message to encrypt or decrypt (or `quit` to quit):')
+    message = input('> ')
+
+    if message.lower() == 'quit':
+        break # Break out of the main program loop.
+
+    # Rotate the letters in message by 13 characters.
+    translated = ''
+    for character in message:
+        if character.isupper():
+            # Concatenate uppercase translated character.
+            transCharIndex = (upperLetters.find(character) + 13) % 26
+            translated += upperLetters[transCharIndex]
+        elif character.islower():
+            # Concatenate lowercase translated character.
+            transCharIndex = (lowerLetters.find(character) + 13) % 26
+            translated += lowerLetters[transCharIndex]
+        else:
+            # Concatenate the character untranslated.
+            translated += character
+
+    # Display the translation:
+    print('The translated message is:')
+    print(translated)
+    print()
+
+    try:
+        # Copy the translation to the clipboard:
+        pyperclip.copy(translated)
+        print('(Copied to clipboard.)')
+    except:
+        pass
