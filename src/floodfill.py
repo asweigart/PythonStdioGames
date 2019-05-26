6c835c2208f6479d1855c7c5597578deeb98e030
Al Sweigart <asweigart@gmail.com>
2019-05-26 10:43:58 -0700

    Finished adding comments to floodfill.

--- src/floodfill.py
+++ src/floodfill.py
@@ -32,9 +32,10 @@ for i in range(NUM_RECTANGLES):
     rectWidth = random.randint(MIN_RECT_SIZE, WIDTH - rectLeft)
     rectHeight = random.randint(MIN_RECT_SIZE, HEIGHT - rectTop)
 
-    # Select a random character for this
+    # Select a random character for this rectangle:
     rectChar = random.choice('!@#*OX')
 
+    # Setup the rectangle to the field data structure:
     for x in range(rectLeft, rectLeft + rectWidth):
         field[(x, rectTop)] = rectChar # Setup the top line.
         field[(x, rectTop + rectHeight - 1)] = rectChar # Setup bottom line.
@@ -47,6 +48,7 @@ for i in range(NUM_RECTANGLES):
 
 def recursiveFloodFill(field, x, y, newChar, curChar=None):
     if curChar == None:
+        # If the current character isn't given, use the char at field[(x, y)].
         curChar = field[(x, y)]
 
     # Base case; just return:
@@ -57,13 +59,18 @@ def recursiveFloodFill(field, x, y, newChar, curChar=None):
 
     # Recursive case; change the neighboring characters:
     if y + 1 < HEIGHT and field[(x, y + 1)] == curChar:
+        # Call recursiveFloodFill() on the southern neighbor:
         recursiveFloodFill(field, x, y + 1, newChar, curChar)
     if y - 1 >= 0 and field[(x, y - 1)] == curChar:
+        # Call recursiveFloodFill() on the northern neighbor:
         recursiveFloodFill(field, x, y - 1, newChar, curChar)
     if x + 1 < WIDTH and field[(x + 1, y)] == curChar:
+        # Call recursiveFloodFill() on the eastern neighbor:
         recursiveFloodFill(field, x + 1, y, newChar, curChar)
     if x - 1 >= 0 and field[(x - 1, y)] == curChar:
+        # Call recursiveFloodFill() on the western neighbor:
         recursiveFloodFill(field, x - 1, y, newChar, curChar)
+
 
 def iterativeFloodFill(field, startx, starty, newChar):
     curChar = field[(startx, starty)]
@@ -72,47 +79,56 @@ def iterativeFloodFill(field, startx, starty, newChar):
 
     while len(coordinatesToChange) > 0:
         x, y = coordinatesToChange.pop()
-
         field[(x, y)] = newChar # Change the character.
 
         # Change the neighboring characters:
         if y + 1 < HEIGHT and field[(x, y + 1)] == curChar:
+            # Change the character for the southern neighbor:
             coordinatesToChange.append((x, y + 1))
         if y - 1 >= 0 and field[(x, y - 1)] == curChar:
+            # Change the character for the northern neighbor:
             coordinatesToChange.append((x, y - 1))
         if x + 1 < WIDTH and field[(x + 1, y)] == curChar:
+            # Change the character for the eastern neighbor:
             coordinatesToChange.append((x + 1, y))
         if x - 1 >= 0 and field[(x - 1, y)] == curChar:
+            # Change the character for the western neighbor:
             coordinatesToChange.append((x - 1, y))
 
+
 def printField(field):
+    # Display the field data structure on the screen:
+
+    # Display the x coordinates along the top:
     print('   ', end='')
     for i in range(WIDTH // 10):
         print(str(i) + NINE_SPACES, end='')
     print()
     print('   ' + ('0123456789' * 10)[:WIDTH])
 
+    # Print the characters in the field:
     for y in range(HEIGHT):
-        # Print each row.
+        # Print each row, including the y coordinates on the left side:
         print(str(y).rjust(2) + ' ', end='')
         for x in range(WIDTH):
-            # Print each column.
+            # Print each character:
             print(field[(x, y)], end='')
-        print()
-    print()
+        print() # Print a newline at the end of the row.
+
 
 while True:
     printField(field)
     print('Enter "[x] [y] [char]" (e.g. "3 3 #", no quotes) or QUIT to quit:')
-
     response = input()
 
+    # Check if user wanted to quit:
     if response.upper() == 'QUIT':
         sys.exit()
 
     response = response.split()
     if len(response) != 3:
         print('Invalid input. Enter something like "3 3 #" (without quotes).')
+        continue
 
     x, y, newChar = response
 
