ea0d98fa73db5ca9b6b208c281ff271ef7acbd88
Al Sweigart <asweigart@gmail.com>
2019-05-28 09:41:22 -0700

    Refactored coin flip

--- src/coinflipsimulator.py
+++ src/coinflipsimulator.py
@@ -13,36 +13,38 @@ while True:
         numberOfFlips = int(numberOfFlips)
         break
 
-print('Flipping a coin %s times...' % (numberOfFlips))
-print('Press Enter to begin...')
-input()
-
 streakStats = {} # Keys are streak lengths, values are frequency.
-currentStreakLength = 0
-currentStreakSide = ''
-
 for i in range(numberOfFlips):
+    isFirstFlip = i == 0
+
     # Simulate one coin flip:
     if random.randint(0, 1) == 0:
-        flip = 'h' # heads
+        flip = 'heads'
     else:
-        flip = 't' # tails
-    print(flip, end='')
+        flip = 'tails'
+    print(flip[0], end='') # Print out "h" or "t".
+
+    if isFirstFlip:
+        currentStreakLength = 0
+        currentStreakSide = flip
 
     # Check if we need to reset the streak:
     if flip != currentStreakSide:
-        if currentStreakLength != 0:
-            # Record the streak stats:
-            streakStats.setdefault(currentStreakLength, 0)
-            streakStats[currentStreakLength] += 1
+        # Record the streak stats:
+        streakKey = (currentStreakLength, currentStreakSide)
+        streakStats.setdefault(streakKey, 0)
+        streakStats[streakKey] += 1
+
+        # Reset the streak length for this new streak:
         currentStreakLength = 1
         currentStreakSide = flip
     else:
         currentStreakLength += 1
 
-# Record the final streak:
-streakStats.setdefault(currentStreakLength, 0)
-streakStats[currentStreakLength] += 1
+# Record the streak stats for the final flip:
+streakKey = (currentStreakLength, currentStreakSide)
+streakStats.setdefault(streakKey, 0)
+streakStats[streakKey] += 1
 
 print()
 print('Simulation finished.')
@@ -50,7 +52,6 @@ streakLengths = list(streakStats.keys())
 streakLengths.sort()
 
 # Display results:
-print('Streak   Number of')
-print('Length - Occurences')
 for streakLength in streakLengths:
-    print(str(streakLength).rjust(6) + ' - ' + str(streakStats[streakLength]))
+    label = str(streakLength[0]) + ' ' + streakLength[1] + ' in a row'
+    print(label.rjust(21) + ' - ' + str(streakStats[streakLength]))
