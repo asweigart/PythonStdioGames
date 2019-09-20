f097171cc300f51204c55204dd6eda4a9f4b5e57
Al Sweigart <asweigart@gmail.com>
2019-09-20 13:14:28 -0700

    Adding alphabetize words quiz and common words file.

---
+++ src/alphabetizewordquiz.py
@@ -0,0 +1,93 @@
+# Alphabetize Word Quiz, by Al Sweigart al@inventwithpython.com
+# A time-based quiz game to see how fast you can alphabetize words.
+
+# EXPERIMENT! Try changing the QUESTION_SIZE and QUIZ_DURATION constants.
+
+import random, time
+
+# Set up the constants:
+QUESTION_SIZE = 3 # Each question shows 3 words to alphabetize.
+WORD_RANGE = 50 # How closely grouped the selected words are.
+QUIZ_DURATION = 30 # The quiz lasts 30 seconds.
+assert QUIZ_DURATION > 0
+
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+REVERSE_ALPHABET = ''.join(sorted(ALPHABET, reverse=True))
+
+# Read in the words from the word file.
+# This file can be downloaded from:
+# https://inventwithpython.com/commonenglishwords.txt
+with open('commonenglishwords.txt') as wordFile:
+    allWords = wordFile.read().splitlines()
+
+
+def slowPrint(text, pauseAmount):
+    for character in text:
+        # Set flush=True here so the text is immediately printed:
+        print(character, flush=True, end='') # end='' means no newline.
+        time.sleep(pauseAmount) # Pause in between each letter.
+    print() # Print a newline.
+
+
+# Fancy animation for the title:
+slowPrint(ALPHABET, 0.05)
+slowPrint('  ALPHABETIZE WORD QUIZ', 0.05)
+slowPrint(REVERSE_ALPHABET, 0.05)
+
+print(f'''
+By Al Sweigart al@inventwithpython.com
+
+To play, enter the alphabetical order of the words shown as fast as
+possible. Try to get as many as possible in {QUIZ_DURATION} seconds!
+
+Example:
+    trade tracks transmit
+    1     2      3
+    > 213
+
+Press enter to start!
+''')
+input() # Let the player press Enter to start the game.
+
+startTime = time.time() # Get the current time for the start time.
+numCorrect = 0 # Number of questions answered correctly.
+while True: # Main game loop.
+    # Come up with QUESTION_SIZE words for the question:
+    #questionLetters = random.sample(ALPHABET, QUESTION_SIZE)
+    wordRange = random.randint(0, len(allWords) - WORD_RANGE)
+    possibleWords = allWords[wordRange:wordRange + WORD_RANGE]
+    questionWords = random.sample(possibleWords, QUESTION_SIZE)
+    random.shuffle(questionWords) # Randomize the order of the words.
+
+    print('   ', ' '.join(questionWords)) # Print the words.
+
+    # Print the number labels:
+    print('    ', end='')
+    for i, word in enumerate(questionWords):
+        print(i + 1, end='')
+        print(' ' * (len(word)), end='')
+    print() # Print a newline.
+
+    response = input('> ').replace(' ', '') # Remove any spaces from input.
+
+    # Check if the quiz's time is up:
+    if time.time() - 30 > startTime:
+        print("TIME'S UP!")
+        break
+
+    # Check if the response is correct:
+    isCorrect = True
+    for i, number in enumerate(response):
+        if questionWords[int(number) - 1] != sorted(questionWords)[i]:
+            isCorrect = False
+
+    if isCorrect:
+        print('    Correct!\n')
+        numCorrect += 1 # Increase the score by 1.
+    else:
+        print('    Ack. :(\n')
+
+# After the loop exits, the quiz is over. Show the final score:
+print(f'In {QUIZ_DURATION} you got {numCorrect} correct!')
+print('Thanks for playing!')
+
--- src/calendarmaker.py
+++ src/calendarmaker.py
@@ -1,6 +1,5 @@
 # Calendar Maker, by Al Sweigart al@inventwithpython.com
-
-# just like the built-in calendar, but this one makes calendars that can be printed and written on.
+# Create monthly calendars, saved to a text file and fit for printing.
 
 import datetime
 
@@ -39,59 +38,64 @@ while True: # Loop to get a month from the user.
     if 1 <= month <= 12:
         break
 
-# Add the month and year label to the calendar:
-calText = '' # calText will contain the string of our calendar page.
-fullCalendarWidth = 8 + (DAY_BOX_SIZE * 7) # There are 8 vertical lines.
-calText += f'{MONTHS[month - 1]} {year}'.center(fullCalendarWidth) + '\n'
+def getCalendarFor(year, month):
+    # Add the month and year label to the calendar:
+    calText = '' # calText will contain the string of our calendar page.
+    fullCalendarWidth = 8 + (DAY_BOX_SIZE * 7) # There are 8 vertical lines.
+    calText += f'{MONTHS[month - 1]} {year}'.center(fullCalendarWidth) + '\n'
 
-# Add the days of the week labels to the calendar:
-calText += ' '
-for day in DAYS:
-    calText += day.center(DAY_BOX_SIZE)
-    if day != DAYS[-1]:
-        calText += ' '
-calText += '\n'
+    # Add the days of the week labels to the calendar:
+    calText += ' '
+    for day in DAYS:
+        calText += day.center(DAY_BOX_SIZE)
+        if day != DAYS[-1]:
+            calText += ' '
+    calText += '\n'
 
-# Create the horizontal lines:
-horizontalLine = (('+' + ('-' * DAY_BOX_SIZE)) * 7) + '+\n'
+    # Create the horizontal lines:
+    horizontalLine = (('+' + ('-' * DAY_BOX_SIZE)) * 7) + '+\n'
 
-# Create the vertical line:
-verticalLine = (('|' + (' ' * DAY_BOX_SIZE)) * 7) + '|\n'
+    # Create the vertical line:
+    verticalLine = (('|' + (' ' * DAY_BOX_SIZE)) * 7) + '|\n'
 
-# Figure out on what day the month starts:
-#breakpoint()
-calDate = datetime.date(year, month, 1)
+    # Figure out on what day the month starts:
+    #breakpoint()
+    calDate = datetime.date(year, month, 1)
 
-while True: # Loop for each week.
-    if calDate.month != month:
-        break # We are done with this month.
-
-    calText += horizontalLine
-    labeledLine = verticalLine
-
-    while True: # Fill in the day labels for this week:
+    while True: # Loop for each week.
         if calDate.month != month:
             break # We are done with this month.
 
-        calDay = (calDate.weekday() + 1) % 7 # Adjust since weekday() has Mon as 0.
-        calLabelIndex = calDay * (DAY_BOX_SIZE + 1) + 1
-        labeledLine = labeledLine[:calLabelIndex] + str(calDate.day).rjust(2) + labeledLine[calLabelIndex+2:]
+        calText += horizontalLine
+        labeledLine = verticalLine
 
-        calDate += ONE_DAY
+        while True: # Fill in the day labels for this week:
+            if calDate.month != month:
+                break # We are done with this month.
 
-        if calDate.weekday() == 6: # If we've reached Sunday, break and start a new week.
-            break
+            calDay = (calDate.weekday() + 1) % 7 # Adjust since weekday() has Mon as 0.
+            calLabelIndex = calDay * (DAY_BOX_SIZE + 1) + 1
+            labeledLine = labeledLine[:calLabelIndex] + str(calDate.day).rjust(2) + labeledLine[calLabelIndex+2:]
 
-    # At this point, we're done with the labeled line.
-    calText += labeledLine
+            calDate += ONE_DAY
 
-    for i in range(DAY_BOX_SIZE // 2):
-        calText += verticalLine
+            if calDate.weekday() == 6: # If we've reached Sunday, break and start a new week.
+                break
 
-# At this point, we're done with the month.
+        # At this point, we're done with the labeled line.
+        calText += labeledLine
 
-calText += horizontalLine
+        for i in range(DAY_BOX_SIZE // 2):
+            calText += verticalLine
 
+    # At this point, we're done with the month.
+
+    calText += horizontalLine
+
+    return calText
+
+
+calText = getCalendarFor(year, month)
 
 # Display the calendar:
 print(calText)
