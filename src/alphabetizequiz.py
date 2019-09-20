3f3b0dcc462e23cfe27fc3e74b8a995c8b658988
Al Sweigart <asweigart@gmail.com>
2019-09-20 13:08:57 -0700

    Fixed a bug in alphabetize quiz that made wrong answers marked as correct.

--- src/alphabetizequiz.py
+++ src/alphabetizequiz.py
@@ -1,21 +1,25 @@
 # Alphabetize Quiz, by Al Sweigart al@inventwithpython.com
+# A time-based quiz game to see how fast you can alphabetize letters.
+
+# EXPERIMENT! Try changing the QUESTION_SIZE and QUIZ_DURATION constants.
 
 import random, time
 
-# Setup the constants:
+# Set up the constants:
 QUESTION_SIZE = 5 # Each question shows 5 letters to alphabetize.
 QUIZ_DURATION = 30 # The quiz lasts 30 seconds.
 assert QUESTION_SIZE <= 26
 assert QUIZ_DURATION > 0
 
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-REVERSE_ALPHABET = 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
+REVERSE_ALPHABET = ''.join(sorted(ALPHABET, reverse=True))
 
 
 def slowPrint(text, pauseAmount):
     for character in text:
-        print(character, flush=True, end='')
-        time.sleep(pauseAmount)
+        # Set flush=True here so the text is immediately printed:
+        print(character, flush=True, end='') # end='' means no newline.
+        time.sleep(pauseAmount) # Pause in between each letter.
     print() # Print a newline.
 
 
@@ -24,15 +28,14 @@ slowPrint(ALPHABET, 0.05)
 slowPrint('    ALPHABETIZE QUIZ', 0.05)
 slowPrint(REVERSE_ALPHABET, 0.05)
 
-print('''
+print(f'''
 By Al Sweigart al@inventwithpython.com
 
 To play, enter the alphabetical order of the letters shown as fast as
-possible. Try to get as many as possible in 30 seconds!
+possible. Try to get as many as possible in {QUIZ_DURATION} seconds!
 
 Example:
     P M O T Q
-
     > mopqt
 
 Press enter to start!
@@ -42,19 +45,19 @@ input() # Let the player press Enter to start the game.
 startTime = time.time() # Get the current time for the start time.
 numCorrect = 0 # Number of questions answered correctly.
 while True: # Main game loop.
-    # Come up with QUESTION_SIZE letters to display:
+    # Come up with QUESTION_SIZE letters for the question:
     questionLetters = random.sample(ALPHABET, QUESTION_SIZE)
     print(' '.join(questionLetters))
     print()
     response = input('> ').upper()
 
-    # Check if the quiz's time is up.
+    # Check if the quiz's time is up:
     if time.time() - 30 > startTime:
         print("TIME'S UP!")
         break
 
     # Check if the response is correct:
-    if sorted(response) == sorted(questionLetters):
+    if list(response) == sorted(questionLetters):
         print('    Correct!\n')
         numCorrect += 1 # Increase the score by 1.
     else:
