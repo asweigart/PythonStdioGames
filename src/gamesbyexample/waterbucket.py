2ca951689f06f4cf3525f4497df742c1c488909e
Al Sweigart <asweigart@gmail.com>
2019-12-10 23:12:03 -0800

    Added graphics to the water bucket game. Removed the OOP version for now.

--- src/gamesbyexample/waterbucket.py
+++ src/gamesbyexample/waterbucket.py
@@ -27,86 +27,128 @@ class Bucket:
         # Put the poured out water into the other bucket:
         intoBucket.water = intoBucket.water + amountToPour
 
-print('WATER BUCKET PUZZLE')
-print('By Al Sweigart al@inventwithpython.com')
-print()
+print('''WATER BUCKET PUZZLE
+By Al Sweigart al@inventwithpython.com
+''')
 
-# Set up the constants used in this program:
-# The bucket sizes available:
-BUCKETS = [{'size': 8, 'water': 0},
-           {'size': 5, 'water': 0},
-           {'size': 3, 'water': 0}]
-LABELS = 'ABC' # Have as many letters as you have buckets.
-GOAL = 4 # The goal amount of water to have.
+GOAL = 4 # The exact amount of water to have in a bucket to win.
+steps = 0 # Keep track of how many steps the player made to solve this.
 
-# Make sure the above settings are valid:
-assert len(BUCKETS) == len(LABELS) == len(set(LABELS))
+# The amount of water in each bucket:
+waterInBucket = {'8': 0, '5': 0, '3': 0}
 
 while True: # Main game loop.
     # Display the current state of the buckets:
-    print('Try to get ' + str(GOAL) + 'L of water with these', len(BUCKETS), 'buckets:')
-    for i, bucket in enumerate(BUCKETS):
-        print('  {}. {}L bucket with {}L of water'.format(LABELS[i], bucket['size'], bucket['water']))
     print()
+    print('Try to get ' + str(GOAL) + 'L of water with these 3 buckets:')
+
+    waterDisplay = [] # Contains strings for water or empty space.
+
+    # Get the strings for the 8L bucket:
+    for i in range(1, 9):
+        if waterInBucket['8'] < i:
+            waterDisplay.append('      ') # Add empty space.
+        else:
+            waterDisplay.append('WWWWWW') # Add water.
+
+    # Get the strings for the 5L bucket:
+    for i in range(1, 6):
+        if waterInBucket['5'] < i:
+            waterDisplay.append('      ') # Add empty space.
+        else:
+            waterDisplay.append('WWWWWW') # Add water.
+
+    # Get the strings for the 3L bucket:
+    for i in range(1, 4):
+        if waterInBucket['3'] < i:
+            waterDisplay.append('      ') # Add empty space.
+        else:
+            waterDisplay.append('WWWWWW') # Add water.
+
+    # Display the buckets with the amount of water in each one:
+    print('''
+8|{7}|
+7|{6}|
+6|{5}|
+5|{4}|  5|{12}|
+4|{3}|  4|{11}|
+3|{2}|  3|{10}|  3|{15}|
+2|{1}|  2|{9}|  2|{14}|
+1|{0}|  1|{8}|  1|{13}|
+ +------+   +------+   +------+
+    8L         5L         3L
+'''.format(*waterDisplay))
 
     # Check if any of the buckets has the goal amount of water:
-    for bucket in BUCKETS:
-        if bucket['water'] == GOAL:
-            print('Good job! You have solved the puzzle!')
+    for waterAmount in waterInBucket.values():
+        if waterAmount == GOAL:
+            print('Good job! You solved it in', steps, 'steps!')
             sys.exit()
 
-    # Let the player select a bucket:
-    while True: # Keep asking until the player enters a valid bucket.
-        print('Select a bucket A to', LABELS[-1], 'or QUIT:')
-        selectedLabel = input().upper()
-
-        if selectedLabel == 'QUIT':
-            print('Thanks for playing!')
-            sys.exit()
-
-        if selectedLabel in tuple(LABELS):
-            break # Player has selected a valid bucket.
-    selectedBucket = BUCKETS[LABELS.index(selectedLabel)]
-
-    # Let the player select an action to do with that bucket:
+    # Let the player select an action to do with a bucket:
     print('You can:')
     print('  (F)ill the bucket')
-    print('  (D)rain the bucket')
-    print('  (P)our the bucket into another')
-    print('  (C)ancel and select another bucket')
+    print('  (E)mpty the bucket')
+    print('  (P)our one bucket into another')
+    print('  (Q)uit')
 
     while True: # Keep asking until the player enters a valid action.
         move = input().upper()
-        if move in ('F', 'D', 'P', 'C'):
+        if move in ('F', 'E', 'P', 'Q'):
             break # Player has selected a valid action.
-        print('Enter F, D, P, or C')
+        print('Enter F, E, P, or Q')
+
+        if move == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+        # At this point, go back to the start of the loop.
+
+    # Let the player select a bucket:
+    while True: # Keep asking until the player enters a valid bucket.
+        print('Select a bucket 8, 5, 3, or QUIT:')
+        srcBucket = input().upper()
+
+        if srcBucket == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+
+        if srcBucket in ('8', '5', '3'):
+            break # Player has selected a valid bucket.
+        # At this point, go back to the start of the loop.
 
     # Carry out the selected action:
     if move == 'F':
         # Set the amount of water to the max size.
-        selectedBucket['water'] = selectedBucket['size']
+        srcBucketSize = int(srcBucket)
+        waterInBucket[srcBucket] = srcBucketSize
+        steps += 1
 
-    elif move == 'D':
-        selectedBucket['water'] = 0 # Set the amount of water to nothing.
+    elif move == 'E':
+        waterInBucket[srcBucket] = 0 # Set water amount to nothing.
+        steps += 1
 
     elif move == 'P':
         # Let the player select a bucket to pour into:
         while True: # Keep asking until the player enters a valid bucket.
-            print('Select a bucket A to', LABELS[-1], 'to pour into:')
-            selectedLabel = input().upper()
-            if selectedLabel in tuple(LABELS):
+            print('Select a bucket to pour into: 8, 5, or 3')
+            dstBucket = input().upper()
+            if dstBucket in ('8', '5', '3'):
                 break # Player has selected a valid bucket.
-        selectedTarget = BUCKETS[LABELS.index(selectedLabel)]
 
         # Figure out the amount to pour:
-        remainingSpace = selectedTarget['size'] - selectedTarget['water']
-        amountToPour = min(remainingSpace, selectedBucket['water'])
+        dstBucketSize = int(dstBucket)
+        emptySpaceInDstBucket = dstBucketSize - waterInBucket[dstBucket]
+        waterInSrcBucket = waterInBucket[srcBucket]
+        amountToPour = min(emptySpaceInDstBucket, waterInSrcBucket)
 
         # Pour out water from this bucket:
-        selectedBucket['water'] = selectedBucket['water'] - amountToPour
+        waterInBucket[srcBucket] -= amountToPour
 
         # Put the poured out water into the other bucket:
-        selectedTarget['water'] = selectedTarget['water'] + amountToPour
+        waterInBucket[dstBucket] += amountToPour
+        steps += 1
 
     elif move == 'C':
         pass # If the player selected Cancel, do nothing.
+
+    # At this point, go back to the start of the main game loop.
