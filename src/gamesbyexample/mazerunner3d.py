b1a1a9b98d0aff3f1428b987b2298bc22c15fc48
Al Sweigart <asweigart@gmail.com>
2019-12-12 08:03:39 -0800

    Added "LIST" command to maze runner programs to view the files they can load.

--- src/gamesbyexample/mazerunner3d.py
+++ src/gamesbyexample/mazerunner3d.py
@@ -187,8 +187,17 @@ By Al Sweigart al@inventwithpython.com
 
 # Get the maze file's filename from the user:
 while True:
-    print('Enter the filename of the maze (or "quit"):')
+    print('Enter the filename of the maze (or LIST or QUIT):')
     filename = input()
+
+    # List all the maze files in the current folder:
+    if filename.upper() == 'LIST':
+        print('Maze files found in', os.getcwd())
+        for fileInCurrentFolder in os.listdir():
+            if (fileInCurrentFolder.startswith('maze') and
+            fileInCurrentFolder.endswith('.txt')):
+                print('  ', fileInCurrentFolder)
+        continue
 
     if filename.upper() == 'QUIT':
         sys.exit()
