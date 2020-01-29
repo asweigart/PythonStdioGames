2e7443034658a0806226d4f24d9be64667cd8171
Al Sweigart <asweigart@gmail.com>
2020-01-28 19:49:49 -0800

    Renaming 'balls' to 'dots' because hurr hurr hurr.

--- src/gamesbyexample/bouncingDots.py
+++ src/gamesbyexample/bouncingDots.py
@@ -1,6 +1,6 @@
-"""Bouncing Ball, by Al Sweigart al@inventwithpython.com
+"""Bouncing Dots, by Al Sweigart al@inventwithpython.com
 
-A bouncing ball animation. Press Ctrl-C to stop."""
+A bouncing dots animation. Press Ctrl-C to stop."""
 __version__ = 1
 
 import sys, random, time
@@ -21,16 +21,16 @@ or a Command Prompt window (on Windows) and running:
 # Setup the constants:
 WIDTH, HEIGHT = bext.size()
 WIDTH -= 1  # Adjustment for Windows Command Prompt.
-NUMBER_OF_BALLS = 35
+NUMBER_OF_DOTS = 35
 COLORS = ('red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white')
-UP_RIGHT = 'ur'
-UP_LEFT = 'ul'
+UP_RIGHT   = 'ur'
+UP_LEFT    = 'ul'
 DOWN_RIGHT = 'dr'
-DOWN_LEFT = 'dl'
+DOWN_LEFT  = 'dl'
 DIRECTIONS = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
-BALL_CHAR = 'O'
+DOT_CHAR  = 'O'
 
-# Key names for ball dictionaries:
+# Key names for dot dictionaries:
 COLOR = 'color'
 X = 'x'
 Y = 'y'
@@ -41,75 +41,75 @@ def main():
     """Run the bouncing dots program."""
     bext.clear()
 
-    # Generate some balls.
-    balls = []
-    for i in range(NUMBER_OF_BALLS):
-        balls.append({COLOR: random.choice(COLORS),
+    # Generate some dots.
+    dots = []
+    for i in range(NUMBER_OF_DOTS):
+        dots.append({COLOR: random.choice(COLORS),
                       X: random.randint(1, WIDTH - 2),
                       Y: random.randint(1, HEIGHT - 2),
                       DIR: random.choice(DIRECTIONS)})
 
     while True:  # Main program loop.
-        oldBallPositions = []
+        oldDotPositions = []
 
-        for ball in balls:
-            # Draw our balls:
-            bext.goto(ball[X], ball[Y])
-            bext.fg(ball[COLOR])
-            print(BALL_CHAR, end='')
+        for dot in dots:
+            # Draw our dots:
+            bext.goto(dot[X], dot[Y])
+            bext.fg(dot[COLOR])
+            print(DOT_CHAR, end='')
 
-            oldBallPositions.append((ball[X], ball[Y]))
+            oldDotPositions.append((dot[X], dot[Y]))
         sys.stdout.flush()  # (Required for bext-using programs.)
         time.sleep(0.1)
 
-        for ball in balls:
-            # Move our balls:
-            if ball[DIR] == UP_RIGHT:
-                ball[X] += 1
-                ball[Y] -= 1
-            elif ball[DIR] == UP_LEFT:
-                ball[X] -= 1
-                ball[Y] -= 1
-            elif ball[DIR] == DOWN_RIGHT:
-                ball[X] += 1
-                ball[Y] += 1
-            elif ball[DIR] == DOWN_LEFT:
-                ball[X] -= 1
-                ball[Y] += 1
+        for dot in dots:
+            # Move our dots:
+            if dot[DIR] == UP_RIGHT:
+                dot[X] += 1
+                dot[Y] -= 1
+            elif dot[DIR] == UP_LEFT:
+                dot[X] -= 1
+                dot[Y] -= 1
+            elif dot[DIR] == DOWN_RIGHT:
+                dot[X] += 1
+                dot[Y] += 1
+            elif dot[DIR] == DOWN_LEFT:
+                dot[X] -= 1
+                dot[Y] += 1
 
-            # See if our balls bounce off the corners:
-            if ball[X] == 0 and ball[Y] == 0:
-                ball[DIR] = DOWN_RIGHT
-            elif ball[X] == 0 and ball[Y] == HEIGHT - 1:
-                ball[DIR] = UP_RIGHT
-            elif ball[X] == WIDTH - 1 and ball[Y] == 0:
-                ball[DIR] = DOWN_LEFT
-            elif ball[X] == WIDTH - 1 and ball[Y] == HEIGHT - 1:
-                ball[DIR] = UP_LEFT
+            # See if our dots bounce off the corners:
+            if dot[X] == 0 and dot[Y] == 0:
+                dot[DIR] = DOWN_RIGHT
+            elif dot[X] == 0 and dot[Y] == HEIGHT - 1:
+                dot[DIR] = UP_RIGHT
+            elif dot[X] == WIDTH - 1 and dot[Y] == 0:
+                dot[DIR] = DOWN_LEFT
+            elif dot[X] == WIDTH - 1 and dot[Y] == HEIGHT - 1:
+                dot[DIR] = UP_LEFT
 
-            # See if our balls bounce off the walls:
-            elif ball[X] == 0 and ball[DIR] == UP_LEFT:
-                ball[DIR] = UP_RIGHT
-            elif ball[X] == 0 and ball[DIR] == DOWN_LEFT:
-                ball[DIR] = DOWN_RIGHT
+            # See if our dots bounce off the walls:
+            elif dot[X] == 0 and dot[DIR] == UP_LEFT:
+                dot[DIR] = UP_RIGHT
+            elif dot[X] == 0 and dot[DIR] == DOWN_LEFT:
+                dot[DIR] = DOWN_RIGHT
 
-            elif ball[X] == WIDTH - 1 and ball[DIR] == UP_RIGHT:
-                ball[DIR] = UP_LEFT
-            elif ball[X] == WIDTH - 1 and ball[DIR] == DOWN_RIGHT:
-                ball[DIR] = DOWN_LEFT
+            elif dot[X] == WIDTH - 1 and dot[DIR] == UP_RIGHT:
+                dot[DIR] = UP_LEFT
+            elif dot[X] == WIDTH - 1 and dot[DIR] == DOWN_RIGHT:
+                dot[DIR] = DOWN_LEFT
 
-            elif ball[Y] == 0 and ball[DIR] == UP_LEFT:
-                ball[DIR] = DOWN_LEFT
-            elif ball[Y] == 0 and ball[DIR] == UP_RIGHT:
-                ball[DIR] = DOWN_RIGHT
+            elif dot[Y] == 0 and dot[DIR] == UP_LEFT:
+                dot[DIR] = DOWN_LEFT
+            elif dot[Y] == 0 and dot[DIR] == UP_RIGHT:
+                dot[DIR] = DOWN_RIGHT
 
-            elif ball[Y] == HEIGHT - 1 and ball[DIR] == DOWN_LEFT:
-                ball[DIR] = UP_LEFT
-            elif ball[Y] == HEIGHT - 1 and ball[DIR] == DOWN_RIGHT:
-                ball[DIR] = UP_RIGHT
+            elif dot[Y] == HEIGHT - 1 and dot[DIR] == DOWN_LEFT:
+                dot[DIR] = UP_LEFT
+            elif dot[Y] == HEIGHT - 1 and dot[DIR] == DOWN_RIGHT:
+                dot[DIR] = UP_RIGHT
 
-        for position in oldBallPositions:
-            # Erase all of the balls.
+        for position in oldDotPositions:
+            # Erase all of the dots.
             bext.goto(position[0], position[1])
             print(' ', end='')
         # At this point, go back to the start of the main program loop.
