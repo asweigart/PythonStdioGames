2e7490359a8a14061dc9c779b56cb39bedea5395
Al Sweigart <asweigart@gmail.com>
2020-03-25 22:23:33 -0700

    Adding bitmap message program.

---
+++ src/gamesbyexample/bitmapmessage.py
@@ -0,0 +1,45 @@
+"""Bitmap Message, by Al Sweigart al@inventwithpython.com
+
+Displays a text message according to the provided bitmap image.
+Tags: tiny, artistic"""
+
+# (!) Try changing this multiline string to any image you like:
+bitmap = """
+               ##   #  ### ##  #
+           # ##########      ##      ####   ###     ######    ###
+  ################### ##    ##      #  # ############################
+  ##################### ## ## #  # ############################## ##
+  ################### ### #   #  ## ##########################  ##
+###      #################       ##############################
+          #############                #### ## ############## #
+           #########            #######   ################ # #
+            ########           ######### #################  #
+  ##         #####  ###      # ############### ######  ## #
+               ####   #       ################   ### ###  #
+                 ########       ## ###########    #  ## ###
+                   ########         ########         #  ### ####
+                   ##########        ######  #        #### ## # ##
+                    #########        ###### ##            ### #   ##
+                     #######          ##### ##          ########   ##
+                    #######           ####              #########
+                    ######             #                 #######   #
+                    ######                                    #    #
+                    ####
+                    ###  #
+                      ##"""
+
+print('Bitmap Message, by Al Sweigart al@inventwithpython.com')
+print('Enter the message you\'d like to display with the bitmap.')
+message = input('> ')
+
+# Loop over each line:
+for line in bitmap.splitlines():
+    # Loop over each character in the line:
+    for i, bit in enumerate(line):
+        if bit == ' ':
+            # Print an empty space since there's a space in the bitmap:
+            print(' ', end='')
+        else:
+            # Print the character from the messge in this space:
+            print(message[i % len(message)], end='')
+    print()  # Print a newline.
