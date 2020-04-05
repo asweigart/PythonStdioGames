2ff87c1697b6f2e35b4de4897b1c93a1253fd882
Al Sweigart <asweigart@gmail.com>
2020-04-05 15:40:22 -0700

    Finished polishing gomoku

--- src/gamesbyexample/gomoku.py
+++ src/gamesbyexample/gomoku.py
@@ -8,15 +8,14 @@ Tags: large, game, board game, two-player"""
 
 import sys
 
-# TODO - polish and refactor
-
 # Set up the constants:
-X_PLAYER = 'x'
-O_PLAYER = 'o'
-EMPTY_SPACE = '.'
-BOARD_WIDTH = 15
-BOARD_HEIGHT = 15
+X_PLAYER = 'x'  # (!) Try changing this to another letter.
+O_PLAYER = 'o'  # (!) Try changing this to another letter.
+EMPTY_SPACE = '.'  # (!) Try changing this to another letter.
+BOARD_WIDTH = 15  # (!) Try changing this to another integer.
+BOARD_HEIGHT = 15  # (!) Try changing this to another integer.
 
+# Let's make sure the board isn't too large to fit on the screen:
 assert BOARD_WIDTH < 100
 assert BOARD_HEIGHT < 100
 
@@ -28,18 +27,29 @@ Gomoku is a Japanese board game where two players take turns placing
 down tiles. The first player to place five tiles in a row horizontally,
 vertically, or diagonally wins.''')
 
-    turn = O_PLAYER
+    turn = O_PLAYER  # O will go first.
     gameBoard = getNewBoard()
     while True:
         displayBoard(gameBoard)
-        makePlayerMove(turn, gameBoard)
+        playerMoveX, playerMoveY = getPlayerMove(turn, gameBoard)
 
-        # Check for winner:
-        if isWinner(turn, gameBoard):
-            displayBoard(gameBoard)
-            print(turn.upper(), 'has won!')
-            print('Thanks for playing!')
-            sys.exit()
+        if (playerMoveX, playerMoveY) != (None, None):
+            # Add this new tile to the board:
+            gameBoard[(playerMoveX, playerMoveY)] = turn
+
+            # Check for winner after this new tile has been added:
+            if isWinner(turn, gameBoard):
+                displayBoard(gameBoard)
+                print(turn.upper(), 'has won!')
+                print('Thanks for playing!')
+                sys.exit()
+
+            # Check for a tie after this new tile has been added:
+            if isBoardFull(gameBoard):
+                displayBoard(gameBoard)
+                print('The board is full and the game is a tie.')
+                print('Thanks for playing!')
+                sys.exit()
 
         # Switch to the other player's turn:
         if turn == O_PLAYER:
@@ -49,6 +59,9 @@ vertically, or diagonally wins.''')
 
 
 def getNewBoard():
+    """Returns a new dictionary that represents the board. The keys are
+    (x, y) tuples of integers starting at 0 and going up to, but not
+    including, BOARD_WIDTH and BOARD_HEIGHT."""
     board = {}
     for x in range(BOARD_WIDTH):
         for y in range(BOARD_HEIGHT):
@@ -56,28 +69,35 @@ def getNewBoard():
     return board
 
 
-def displayBoard(board):
-    # Display the letter labels at the top:
+def displayLetterLabels():
+    """Display a row of letter labels, based on the BOARD_WIDTH."""
     print('   ', end='')  # Print the indentation.
     for x in range(BOARD_WIDTH):
         print(chr(65 + x) + ' ', end='')  # Print the letter.
     print()  # Print a newline.
+
+
+def displayBoard(board):
+    """Display the board data structure on the screen."""
+    displayLetterLabels()  # Display the letter labels at the top.
 
     # Display each board row:
     for y in range(BOARD_HEIGHT):
-        print(str(y + 1).rjust(2) + ' ', end='')  # Print the leftside number label.
+        # Display the leftside number label:
+        print(str(y + 1).rjust(2) + ' ', end='')
         for x in range(BOARD_WIDTH):
+            # Display each tile in this row, followed by a space:
             print(board[(x, y)] + ' ', end='')
+        # Display the rightside number label:
         print(y + 1)
 
-    # Display the letter labels at the bottom:
-    print('   ', end='')  # Print the indentation.
-    for x in range(BOARD_WIDTH):
-        print(chr(65 + x) + ' ', end='')  # Print the letter.
-    print()  # Print a newline.
+    displayLetterLabels()  # Display the letter labels at the bottom.
 
 
-def makePlayerMove(player, board):
+def getPlayerMove(player, board):
+    """Asks the player for a move and returns a (x, y) tuple of integer
+    indexes for the place they want to put their tile. Also returns
+    (None, None) if they want to pass on their turn."""
     print('It is ' + player.upper() + '\'s turn.')
     while True:  # Keep looping until the player enters a valid move:
         print('Enter a move (such as B3) or PASS or QUIT:')
@@ -88,19 +108,29 @@ def makePlayerMove(player, board):
             sys.exit()
 
         if response == 'PASS':
-            return  # Return without making any move.
+            return (None, None)  # No move should be made.
 
+        # Make sure the player entered a valid move like 'B3' or 'D14'.
+        # The response[0] character will always be a letter, and the
+        # next one or more characters must be a number.
         if len(response) >= 2 and response[1:].isdecimal():
-            if ('A' <= response[0] < chr(65 + BOARD_WIDTH)) and (1 <= int(response[1:]) <= BOARD_HEIGHT):
-                x = ord(response[0]) - 65
-                y = int(response[1:]) - 1
-                if board[(x, y)] != EMPTY_SPACE:
-                    print('There is already a piece there.')
-                    continue
-                break  # Player has entered a valid move.
+            # Make sure the letter they entered is on the board:
+            if 'A' <= response[0] < chr(65 + BOARD_WIDTH):
+                # Make sure the number they entered is on the board:
+                if 1 <= int(response[1:]) <= BOARD_HEIGHT:
+                    # Get the integer indexes for their move:
+                    moveX = ord(response[0]) - 65
+                    moveY = int(response[1:]) - 1
+
+                    if board[(moveX, moveY)] != EMPTY_SPACE:
+                        print('There is already a piece there.')
+                        continue
+                    break  # Player has entered a valid move.
+
+        # If any of the previous checks failed, make the player enter
+        # their move again:
         print('That is not a valid space on this board.')
-    # Make the move on the board.
-    board[(x, y)] = player
+    return (moveX, moveY)
 
 
 def isWinner(player, board):
@@ -150,6 +180,17 @@ def isWinner(player, board):
             if tile1 == tile2 == tile3 == tile4 == tile5 == player:
                 return True
     return False
+
+
+def isBoardFull(board):
+    """Returns True if the board is full of tiles, otherwise, returns
+    False if there is at least one empty space somewhere on the board."""
+    for x in range(BOARD_WIDTH):
+        for y in range(BOARD_HEIGHT):
+            if board[(x, y)] == EMPTY_SPACE:
+                # An empty space means the board is not full.
+                return False
+    return True  # None of the spaces were empty, so the board is full.
 
 
 # If the program is run (instead of imported), run the game:
