9367ab35ec1a2a26cfb0c5ee63030c198558ab23
Al Sweigart <asweigart@gmail.com>
2020-04-18 14:25:24 -0700

    Refactoring with changes from copying insights.

--- src/gamesbyexample/caesarcipher.py
+++ src/gamesbyexample/caesarcipher.py
@@ -9,9 +9,14 @@ try:
 except ImportError:
     pass  # If pyperclip is not installed, do nothing. It's no big deal.
 
+# Every possible symbol that can be encrypted/decrypted:
+# (!) You can add numbers and punctuation marks to encrypt those
+# symbols as well.
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 print('Caesar Cipher, by Al Sweigart al@inventwithpython.com')
 
-# Let the user specify if they are encrypting or decrypting:
+# Let the user enter if they are encrypting or decrypting:
 while True:  # Keep asking until the user enters e or d.
     print('Do you want to (e)ncrypt or (d)ecrypt?')
     response = input('> ').lower()
@@ -23,9 +28,9 @@ while True:  # Keep asking until the user enters e or d.
         break
     print('Please enter the letter e or d.')
 
-# Let the user specify the key to use:
+# Let the user enter the key to use:
 while True:  # Keep asking until the user enters a valid key.
-    print('Please specify the key (1 to 26) to use.'.format(mode))
+    print('Please enter the key (0 to {}) to use.'.format(len(SYMBOLS)))
     response = input('> ').upper()
     if not response.isdecimal():
         print('This key is not a number.')
@@ -34,38 +39,35 @@ while True:  # Keep asking until the user enters a valid key.
         key = int(response)
         break
 
-# Let the user specify the message to encrypt/decrypt:
+# Let the user enter the message to encrypt/decrypt:
 print('Enter the message to {}.'.format(mode))
 message = input('> ')
 
 # Caesar cipher only works on uppercase letters:
 message = message.upper()
 
-# Every possible symbol that can be encrypted/decrypted:
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 # Stores the encrypted/decrypted form of the message:
 translated = ''
 
 # Encrypt/decrypt each symbol in the message:
 for symbol in message:
-    if symbol in LETTERS:
+    if symbol in SYMBOLS:
         # Get the encrypted (or decrypted) number for this symbol.
-        num = LETTERS.find(symbol)  # Get the number of the symbol.
+        num = SYMBOLS.find(symbol)  # Get the number of the symbol.
         if mode == 'encrypt':
             num = num + key
         elif mode == 'decrypt':
             num = num - key
 
         # Handle the wrap-around if num is larger than the length of
-        # LETTERS or less than 0:
-        if num >= len(LETTERS):
-            num = num - len(LETTERS)
+        # SYMBOLS or less than 0:
+        if num >= len(SYMBOLS):
+            num = num - len(SYMBOLS)
         elif num < 0:
-            num = num + len(LETTERS)
+            num = num + len(SYMBOLS)
 
         # Add encrypted/decrypted number's symbol to translated:
-        translated = translated + LETTERS[num]
+        translated = translated + SYMBOLS[num]
     else:
         # Just add the symbol without encrypting/decrypting:
         translated = translated + symbol
@@ -75,6 +77,6 @@ print(translated)
 
 try:
     pyperclip.copy(translated)
-    print('Full %sed text copied to clipboard.' % (mode))
+    print('Full {}ed text copied to clipboard.'.format(mode))
 except:
     pass  # Do nothing if pyperclip wasn't installed.
--- src/gamesbyexample/caesarhacker.py
+++ src/gamesbyexample/caesarhacker.py
@@ -13,27 +13,27 @@ print('Enter the encrypted Caesar cipher message to hack.')
 message = input('> ')
 
 # Every possible symbol that can be encrypted/decrypted:
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# (This must match the SYMBOLS used when encrypting the message.)
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-for key in range(len(LETTERS)):  # Loop through every possible key.
+for key in range(len(SYMBOLS)):  # Loop through every possible key.
     translated = ''
 
     # Decrypt each symbol in the message:
     for symbol in message:
-        if symbol in LETTERS:
-            # Get the encrypted (or decrypted) number for this symbol.
-            num = LETTERS.find(symbol)  # Get the number of the symbol.
+        if symbol in SYMBOLS:
+            num = SYMBOLS.find(symbol)  # Get the number of the symbol.
             num = num - key  # Decrypt the number.
 
             # Handle the wrap-around if num is less than 0:
             if num < 0:
-                num = num + len(LETTERS)
+                num = num + len(SYMBOLS)
 
             # Add decrypted number's symbol to translated:
-            translated = translated + LETTERS[num]
+            translated = translated + SYMBOLS[num]
         else:
-            # Just add the symbol without encrypting/decrypting:
+            # Just add the symbol without decrypting:
             translated = translated + symbol
 
     # Display the key being tested, along with its decrypted text:
-    print('Key #%s: %s' % (key, translated))
+    print('Key #{}: {}'.format(key, translated))
